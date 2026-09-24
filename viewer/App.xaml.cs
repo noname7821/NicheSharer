bd94fd657eb29e91928233f6b0ffd4ec7350945d
mintoo <kaanistgut46@gmail.com>
2026-09-25 00:37:25 +0200

    startup log and centered window

--- viewer/App.xaml.cs
+++ viewer/App.xaml.cs
@@ -1,8 +1,36 @@
+using System;
+using System.IO;
 using System.Windows;
 
 namespace NicheShareViewer;
 
-// App entry, nothing special.
+// App entry, logs startup and crashes to startup.log next to the exe.
 public partial class App : Application
 {
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        Log("start");
+        DispatcherUnhandledException += (_, args) =>
+        {
+            Log("crash: " + args.Exception);
+        };
+        base.OnStartup(e);
+    }
+
+    protected override void OnExit(ExitEventArgs e)
+    {
+        Log($"exit {e.ApplicationExitCode}");
+        base.OnExit(e);
+    }
+
+    private static void Log(string msg)
+    {
+        try
+        {
+            var dir = AppDomain.CurrentDomain.BaseDirectory;
+            File.AppendAllText(Path.Combine(dir, "startup.log"),
+                $"{DateTime.Now:T} {msg}{Environment.NewLine}");
+        }
+        catch { /* ignore */ }
+    }
 }
